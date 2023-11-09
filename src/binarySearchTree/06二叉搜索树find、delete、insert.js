9e8d2eaf71e01a2c4bf7ed57c27e677db0af8e55
yuancan <can.yuan@mintegral.com>
2023-11-09 20:42:25 +0800

    fix: 二叉搜索树插入操作

--- src/binarySearchTree/06二叉搜索树find、delete、insert.js
+++ src/binarySearchTree/06二叉搜索树find、delete、insert.js
@@ -1,67 +1,90 @@
-class Node {
+class TreeNode {
   constructor(val) {
-    this.val = val !== void 0 ? val : null
-    this.left = null
-    this.right = null
+    this.val = val !== void 0 ? val : null;
+    this.left = null;
+    this.right = null;
   }
 }
 
 function Tree(val) {
-  this.root = new Node(val)
+  this.root = null;
 }
 
 Tree.prototype.find = function (val) {
-  return travser(this.root, val)
+  return this.traverse(this.root, val);
 }
 
-function travser(node, val) {
-  if (!node) return null
-  if (node.val === val) return node
-  else if (node.val < val) return travser(node.right, val)
-  else if (node.val > val) return travser(node.left, val)
+Tree.prototype.traverse = function traverse(node, val) {
+  if (node === null) {
+    return null;
+  }
+
+  if (node.val === val) {
+    return node;
+  } else if (node.val < val) {
+    return this.traverse(node.right, val);
+  } else if (node.val > val) {
+    return this.traverse(node.left, val);
+  }
 }
 
 Tree.prototype.insert = function (val) {
-  travserInsert(this.root, val)
+  this.root = this.traverseInsert(this.root, val);
 }
 
-function travserInsert(node, val) {
-  if (node === null) {
-    return new Node(val);
+Tree.prototype.traverseInsert = function (root, val) {
+  if (root === null) {
+    return new TreeNode(val);
   }
-  if (node.val === val) {
-    return;
-  } else if (node.val < val) {
-    travserInsert(node.right, val)
-  } else {
-    travserInsert(node.left, val)
+
+  let pos = root;
+  while (pos !== null) {
+    if (val < pos.val) {
+      if (pos.left === null) {
+        pos.left = new TreeNode(val);
+        break;
+      } else {
+        pos = pos.left;
+      }
+    } else {
+      if (pos.right === null) {
+        pos.right = new TreeNode(val);
+        break;
+      } else {
+        pos = pos.right;
+      }
+    }
   }
+  return root;
 }
 
 Tree.prototype.delete = function (val) {
-  let node = this.find(val)
-  if (!node) return this.root
+  let node = this.find(val);
+  if (node === null) {
+    return this.root;
+  }
+
   if (!node.left && !node.right) {
-    node.val = null
+    node.val = null;
   } else if (node.left) {
-    node = node.left
+    node = node.left;
   } else if (node.right) {
-    node = node.right
+    node = node.right;
   } else {
     // 左分支的最右侧节点
-    let left = node.left
+    let left = node.left;
     while (left.right) {
-      left = left.right
+      left = left.right;
     }
-    const lastVal = left.val
+    const lastVal = left.val;
     // 有分支的最左侧节点
     // let right = node.right
     // while (right.left) {
     //   right = right.left
     // }
     // const lastVal = right.val
-    this.delete(lastVal)
-    node.val = lastVal
+    this.delete(lastVal);
+    node.val = lastVal;
   }
 }
 
