aa7e280cfd506cf14c4bb2f46162a44f11578860
yuanvolcano <yuanvolcano@163.com>
2023-11-26 18:23:00 +0800

    chore: 排序算法改进

--- src/sort/01bubble.js
+++ src/sort/01bubble.js
@@ -1,15 +1,23 @@
 // 冒泡排序
 
 const bubbleSort = function (arr) {
-  const len = arr.length
+  const len = arr.length;
+  if (len <= 1) {
+    return arr;
+  }
+
   for (let i = 0; i < len; i++) {
+    // 提前退出冒泡循环的标志位
+    let flag = false;
     for (let j = i + 1; j < len - i; j++) {
       if (arr[i] > arr[j]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]]
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        flag = true;
       }
     }
+    if (!flag) return arr;
   }
-  return arr
+  return arr;
 }
 
 module.exports = bubbleSort
