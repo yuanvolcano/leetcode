ca13241ebd9f48eaf3a799012b5b16341a9c8288
yuancan <can.yuan@mintegral.com>
2025-05-23 16:32:59 +0800

    chore: 优化链表节点删除函数的代码格式，提高可读性

--- src/listNode/06删除链表中的节点.js
+++ src/listNode/06删除链表中的节点.js
@@ -1,13 +1,16 @@
 const deleteNode = function (head, val) {
-  let p = head, pre = null;
+  let p = head,
+    pre = null;
 
-    while (p && p.val !== val) {
-        pre = p;
-        p = p.next;
-    }
-    if (pre === null) {
-        return head.next;
-    }
-    pre.next = p.next;
-    return head;
-}
+  while (p && p.val !== val) {
+    pre = p;
+    p = p.next;
+  }
+
+  if (pre === null) {
+    return head.next;
+  }
+
+  pre.next = p.next;
+  return head;
+};
