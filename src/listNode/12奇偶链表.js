4400ee8641fdcfeefdd20fcc2dc9e551e8e02d6c
yuancan <can.yuan@mintegral.com>
2025-05-23 17:23:56 +0800

    feat: 实现链表奇偶节点重排功能，优化代码格式和可读性

--- src/listNode/12奇偶链表.js
+++ src/listNode/12奇偶链表.js
@@ -1,3 +1,11 @@
+// 给定单链表的头节点 head ，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。
+
+// 第一个节点的索引被认为是 奇数 ， 第二个节点的索引为 偶数 ，以此类推。
+
+// 请注意，偶数组和奇数组内部的相对顺序应该与输入时保持一致。
+
+// 你必须在 O(1) 的额外空间复杂度和 O(n) 的时间复杂度下解决这个问题。
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -5,57 +13,21 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
-function ListNode(val, next) {
-  this.val = (val === undefined ? 0 : val)
-  this.next = (next === undefined ? null : next)
-}
-
-var oddEvenList = function (head) {
-  if (!head || !head.next) {
-    return head
-  }
-  let temp1 = head, count = 0
-  let odd = new ListNode(0), even = new ListNode(0)
-  let temp2 = odd, temp3 = even
-  while (temp1) {
-    if (!(count % 2)) {
-      temp2.next = temp1
-      temp2 = temp2.next
-    } else {
-      temp3.next = temp1
-      temp3 = temp3.next
-    }
-    count++
-    temp1 = temp1.next
-  }
-  if (count % 2) {
-    temp3.next = null
-  } else {
-    temp2.next = null
-  }
-  temp2.next = even.next
-
-  return odd.next
-};
 
 const oddEvenList = function (head) {
-  if (!head) return head
-  let evenHead = head.next
-  let odd = head, even = evenHead
+  if (!head) return head;
+  let evenHead = head.next;
+  let odd = head,
+    even = evenHead;
   while (even !== null && even.next !== null) {
-    odd.next = even.next
-    odd = odd.next
-    even.next = odd.next
-    even = even.next
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
   }
-  odd.next = evenHead
-  return head
-}
+  odd.next = evenHead;
+  return head;
+};
 
 const head = {
   val: 1,
@@ -67,12 +39,12 @@ const head = {
         val: 4,
         next: {
           val: 5,
-          next: null
-        }
-      }
-    }
-  }
-}
+          next: null,
+        },
+      },
+    },
+  },
+};
 
-const result = oddEvenList(head)
-console.log(result)
+const result = oddEvenList(head);
+console.log(result);
--- src/sort/03insert.js
+++ src/sort/03insert.js
@@ -2,14 +2,14 @@
 
 const insertSort = function (arr) {
   for (let i = 1, len = arr.length; i < len; i++) {
-    let curItem = arr[i]
-    let preIndex = i - 1
+    let curItem = arr[i];
+    let preIndex = i - 1;
     while (preIndex >= 0 && arr[preIndex] > curItem) {
-      arr[preIndex + 1] = arr[preIndex]
-      preIndex--
+      arr[preIndex + 1] = arr[preIndex];
+      preIndex--;
     }
-    arr[preIndex + 1] = curItem
+    arr[preIndex + 1] = curItem;
   }
-}
+};
 
-module.exports = insertSort
+module.exports = insertSort;
