5a2a239563c7141c8d93fe8e3b3cbc47f7aff6ce
yuancan <can.yuan@mintegral.com>
2025-05-28 15:42:12 +0800

    feat: 增加 TreeNode 导入，优化最大二叉树构建函数，调整代码格式，提升可读性

--- src/binaryTree/16最大二叉树.js
+++ src/binaryTree/16最大二叉树.js
@@ -1,3 +1,4 @@
+import { TreeNode } from '../../utils/util';
 // 给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
 
 // 创建一个根节点，其值为 nums 中的最大值。
@@ -19,33 +20,28 @@
  */
 var constructMaximumBinaryTree = function (nums) {
   if (!nums.length) {
-    return null
+    return null;
   }
-  const maxIndex = getMaxIndex(nums, 0, nums.length - 1)
-  const root = new TreeNode(nums[maxIndex])
-  root.left = constructMaximumBinaryTree(nums.slice(0, maxIndex))
-  root.right = constructMaximumBinaryTree(nums.slice(maxIndex + 1))
-  return root
+  const maxIndex = getMaxIndex(nums, 0, nums.length - 1);
+  const root = new TreeNode(nums[maxIndex]);
+  root.left = constructMaximumBinaryTree(nums.slice(0, maxIndex));
+  root.right = constructMaximumBinaryTree(nums.slice(maxIndex + 1));
+  return root;
 };
 
-function TreeNode(val, left, right) {
-  this.val = (val === undefined ? 0 : val)
-  this.left = (left === undefined ? null : left)
-  this.right = (right === undefined ? null : right)
-}
-
 function getMaxIndex(nums, start, end) {
-  let max = nums[start], maxIndex = start
+  let max = nums[start],
+    maxIndex = start;
   while (start <= end) {
     if (nums[start] > max) {
-      max = nums[start]
-      maxIndex = start
+      max = nums[start];
+      maxIndex = start;
     }
-    start++
+    start++;
   }
-  return maxIndex
+  return maxIndex;
 }
 
-const list = [3, 2, 1, 6, 0, 5]
-const result = constructMaximumBinaryTree(list)
-console.log(result)
+const list = [3, 2, 1, 6, 0, 5];
+const result = constructMaximumBinaryTree(list);
+console.log(result);
