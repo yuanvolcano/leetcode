fdce6fba8de8fc5b9d205208862755af7ee6a76f
yuancan <can.yuan@mintegral.com>
2025-05-28 16:45:18 +0800

    feat: 修复构建树函数的条件判断，优化节点创建方式，提升代码可读性

--- src/binaryTree/05从前序遍历和中序遍历构造二叉树.js
+++ src/binaryTree/05从前序遍历和中序遍历构造二叉树.js
@@ -1,15 +1,17 @@
 // 先序：考察到一个节点后，即刻输出该节点的值，并继续遍历其左右子树。(根左右)
 
+import { TreeNode } from '../../utils/util';
+
 // 中序：考察到一个节点后，将其暂存，遍历完左子树后，再输出该节点的值，然后遍历右子树。(左根右)
 
 // 后序：考察到一个节点后，将其暂存，遍历完左右子树后，再输出该节点的值。(左右根)
 
 const buildTree = function (preOrder, inOrder) {
-  if (preOrder.length === 0 || (inOrder.length === 0 && preOrder.length !== inOrder.length)) {
+  if (preOrder.length === 0 || inOrder.length === 0 || preOrder.length !== inOrder.length) {
     return null;
   }
-  let root = {};
-  root.val = preOrder[0];
+
+  const root = new TreeNode(preOrder[0]);
 
   let rootIndex = inOrder.indexOf(root.val);
   let inLeftChildTree = inOrder.slice(0, rootIndex);
