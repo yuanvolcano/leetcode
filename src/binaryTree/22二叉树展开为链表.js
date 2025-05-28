80c00e10a8d5f72349dbd8b0347dfa598a9d1575
yuancan <can.yuan@mintegral.com>
2025-05-28 15:31:03 +0800

    feat: 优化二叉树拉平函数，增加详细注释，提升代码可读性

--- src/binaryTree/22二叉树展开为链表.js
+++ src/binaryTree/22二叉树展开为链表.js
@@ -10,21 +10,27 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
+
 var flatten = function (root) {
-  if (root === null) return
+  // 定义：将以 root 为根的树拉平为链表
+  // base case
+  if (root === null) return;
 
   // 把左右子树拉平
-  flatten(root.left)
-  flatten(root.right)
+  flatten(root.left);
+  flatten(root.right);
 
-  let left = root.left
-  let right = root.right
+  // *** 后序遍历位置 ***
+  // 1、左右子树已经被拉平成一条链表
+  let left = root.left;
+  let right = root.right;
 
-  // 将左子树作为右子树
-  root.left = null
-  root.right = left
+  // 2、将左子树作为右子树
+  root.left = null;
+  root.right = left;
 
-  let temp = root
+  // 3、将原先的右子树接到当前右子树的末端
+  let temp = root;
   while (temp.right != null) {
     temp = temp.right;
   }
